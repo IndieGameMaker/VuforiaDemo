6b3100b443adf3d319e69e10ae5ea8a5d39ac2b7
Jaehyun Lee <myleje@gmail.com>
2019-09-29 09:56:27 +0900

    Initial commit

---
+++ Assets/Scripts/Fly.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fly : MonoBehaviour
+{
+    public Animation butterfly;
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.CompareTag("TIGER"))
+        {
+            butterfly.enabled = true;
+        }
+        else
+        {
+            butterfly.enabled = false;
+        }
+    }
+}
---
+++ Assets/Scripts/TouchMgr.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchMgr : MonoBehaviour
+{
+    private Ray ray;
+    private RaycastHit hit;
+    private Camera arCam;
+    public GameObject canvasObj;
+
+    void Start()
+    {
+        arCam = Camera.main; // GetComponent<Camera>();        
+    }
+
+    void Update()
+    {
+        ray = arCam.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(ray.origin, ray.direction * 10.0f, Color.green);
+
+        if (Input.GetMouseButtonDown(0)
+            && Physics.Raycast(ray , out hit, 10.0f))
+        {
+            if (hit.collider.CompareTag("TIGER"))
+            {
+                canvasObj.SetActive(!canvasObj.activeSelf);
+            }            
+        }
+    }
+}
